27c0479176e512136f98ed64ac185f21ad144064
chenfuxing <chenfuxing@theforceprotocol.com>
2020-10-29 14:14:12 +0800

    add blacklist support

--- contracts/KUN.sol
+++ contracts/KUN.sol
@@ -9,13 +9,51 @@ contract KUN is ERC20Capped, MinterControl, VersionedInitializable {
 
     uint256 public constant maxSupply = 12000000000000000000000000;
 
-    function getRevision() internal override pure returns (uint256) {
-        return uint256(0x1);
-    }
+    event DestroyedBlackFunds(address account, uint256 balance);
+    event AddedBlackList(address account);
+    event RemovedBlackList(address account);
+
+    mapping(address => bool) public isBlackListed;
 
     function initialize(address admin) public initializer {
         initializeMinterControl(admin);
         initializeERC20Capped("QIAN governance token", "KUN", 18, maxSupply);
+    }
+    
+    function addBlackList(address account) public onlyAdmin {
+        isBlackListed[account] = true;
+        emit AddedBlackList(account);
+    }
+
+    function removeBlackList(address account) public onlyAdmin {
+        isBlackListed[account] = false;
+        emit RemovedBlackList(account);
+    }
+
+    function destroyBlackFunds(address account) public onlyAdmin {
+        require(isBlackListed[account], "account not in blacklist");
+        uint256 dirtyFunds = balanceOf(account);
+        _burn(account, dirtyFunds);
+        emit DestroyedBlackFunds(account, dirtyFunds);
+    }
+
+    function transfer(address recipient, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        require(!isBlackListed[msg.sender], "blacklist");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        require(!isBlackListed[sender], "blacklist");
+        return super.transferFrom(sender, recipient, amount);
     }
 
     function mint(address account, uint256 amount) public onlyMinter {
@@ -40,5 +78,9 @@ contract KUN is ERC20Capped, MinterControl, VersionedInitializable {
         );
         _approve(account, _msgSender(), decreasedAllowance);
         _burn(account, amount);
+    }
+
+    function getRevision() internal override pure returns (uint256) {
+        return uint256(0x1);
     }
 }
